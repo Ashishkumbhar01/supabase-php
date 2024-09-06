82ed57a4f312a2384f5380bdbad2c05b61d2a94f
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-09-06 11:37:31 +0530

    update

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
+use Supabase\Supabase\Supabase as Supabase;
 
 require_once __DIR__."/vendor/autoload.php";
 
-$client = new Supabase();
+$url = "https://qbsctlzwkqcigickngub.supabase.co";
+$key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFic2N0bHp3a3FjaWdpY2tuZ3ViIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjM4ODUxNjgsImV4cCI6MjAzOTQ2MTE2OH0.rwtjirssp6sdBSVGrwlqh8dDoQmoJOUEa3LUAFfn1SQ";
+
+$client = new Supabase($url, $key, "users");
 
 echo "<pre>";
-print_r($client);
+//print_r($client);
+$data = $client->get("users");
 
+print_r($data);
