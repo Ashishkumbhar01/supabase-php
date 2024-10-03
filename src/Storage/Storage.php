36cd51d15c2bb69fed1ef9ed6d5e689a25d06260
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-10-04 05:20:09 +0530

    Supabase Storage

--- src/Storage/Storage.php
+++ src/Storage/Storage.php
@@ -3,5 +3,38 @@ namespace Supabase\Storage;
 
 class Storage
 {
-  // Storage class 
+  private $files;
+
+  private function FilesType($files,$table)
+  {
+    if(!isset($files)){
+      return "File not selected.";
+    }
+    if (!isset($table)) {
+      return "please provide Table name.";
+    }
+
+    $extenions = ['txt','pdf','ppt','json','csv','mp3','mp4','ogg','webp','png','jpg','jpeg','gif','svg'];
+
+  }
+  
+  public function uploadImages()
+  {
+    // Images
+  }
+
+  public function uploadVideos()
+  {
+    // Videos
+  }
+
+  public function uploadAudios()
+  {
+    // Audios
+  }
+
+  public function uploadFiles()
+  {
+    // files
+  }
 }
