f150e5de6d9da9d43ba60bb63fea1d819b0ffe74
Sushil Kumar <sushilkumbhar77@gmail.com>
2024-10-04 07:04:41 +0530

    Supabase Storage added (#23)
    * update
    
    * Supabase Storage
    
    * Supabase Storage
    
    * Create FUNDING.yml
    
    * Update FUNDING.yml

--- src/Storage/Storage.php
+++ src/Storage/Storage.php
@@ -1,7 +1,42 @@
 <?php
 namespace Supabase\Storage;
 
-class Storage
+use Supabase\Supabase\Supabase;
+
+class Storage extends Supabase
 {
-  // Storage class 
+  private $files;
+
+  private function FilesType($files,$table)
+  {
+    if(!isset($files)){
+      return "File not selected.";
+    }
+    if (!isset($table)) {
+      return "please provide Table name.";
+    }
+
+    $extensions = ['txt', 'pdf', 'pptx', 'xlsx', 'docs', 'zip', 'csv', 'mp3', 'mp4', 'ogg', 'webp', 'png', 'jpg', 'jpeg', 'gif', 'svg'];
+
+  }
+  
+  public function uploadImages()
+  {
+    // Images
+  }
+
+  public function uploadVideos()
+  {
+    // Videos
+  }
+
+  public function uploadAudios()
+  {
+    // Audios
+  }
+
+  public function uploadFiles()
+  {
+    // files
+  }
 }
