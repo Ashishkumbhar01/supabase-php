fd7ac9d35ea80253873509c74deff3c00a188391
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-04-19 08:48:43 +0530

    test pass

--- tests/SupabaseTest.php
+++ tests/SupabaseTest.php
@@ -6,6 +6,7 @@ use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Supabase\Client\Functions as Supabase;
+use Dotenv\Dotenv;
 
 #[CoversClass(Functions::class)]
 class SupabaseTest extends TestCase
@@ -13,7 +14,15 @@ class SupabaseTest extends TestCase
     #[Test]
     public function connected()
     {
-        $client = new Supabase('https://uafnqazxrhgsrnxxazkg.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVhZm5xYXp4cmhnc3JueHhhemtnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDAwOTM4MDQsImV4cCI6MjA1NTY2OTgwNH0.2FMd5u8-K7R6gSUFAp3SDzt766hGGHTVGRkdyNwGLAM');
+        $dotenv = Dotenv::createImmutable(__DIR__);
+        $dotenv->safeLoad();
+
+        $config = [
+            'url' => $_ENV['URL'],
+            'apikey' => $_ENV['APIKEY']
+        ];
+
+        $client = new Supabase($config['url'], $config['apikey']);
         $this->assertInstanceOf(Supabase::class, $client);
        // $this->assertTrue($client->getAllData('Users'));
     }
